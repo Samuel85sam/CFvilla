490323a9f801e349a0c31ba734e1292fe9fc7c6c
Samuel <Samueldemees@gmail.com>
2024-01-11 11:25:21 +0100

    .env + gitignore

--- CFvillaBack/server.js
+++ CFvillaBack/server.js
@@ -15,7 +15,7 @@ const cors = require('cors');
 
 const app = express();
 async function server() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test')
+    await mongoose.connect(process.env.DB_CONNECT)
     console.log('===> mongoose.connect OK');
 }
 server()

9eaf311246e50c5b03cf7bdd960d3d7c72594ee1
Samuel <Samueldemees@gmail.com>
2024-02-02 15:18:47 +0100

    front card

--- CFvillaFront/src/routes/public-interface/front-blog.jsx
+++ CFvillaFront/src/routes/public-interface/front-blog.jsx
@@ -1,42 +1,85 @@
-import { React, useState, useEffect } from 'react'
+import { useEffect, useState } from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import CRUD from '../../business/api-requests/CRUD';
+
 
 
 const FrontBlog = () => {
     const [posts, setpost] = useState([]);
     console.log('posts => blog ', posts);
 
-    const allPosts = async () => {
+    const fetchPosts = async () => {
         // const posts = await CRUD.getForm('posts/', { populate: ['img', 'author'] })
-        const posts = await CRUD.getForm('posts/');
-        setpost(posts)
+        const fetchPosts = await CRUD.getForm('posts/');
+        setpost(fetchPosts)
     };
 
     useEffect(() => {
-        allPosts()
+        fetchPosts()
     }, []);
+
+    // const bull = (
+    //     <Box
+    //         component="span"
+    //         sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    //     >
+
+    //     </Box>
+    // );
+
+
+
 
 
     return (
         <>
-            <h1>FrontBlog</h1>
-            <h2>posts : </h2>
             <div>
                 <ul>
                     {posts.map((post, index) => (
                         <li
                             key={index}
                         >
-                            <h3>
-                                {post.title}
-                            </h3>
-                            <p>
-                                {post.body}
-                            </p>
+                            <Box component="span"
+                                sx={{
+                                    minWidth: "600px",
+                                    width:'50%' ,
+                                    display: 'inline-block',
+                                    mx: '2px',
+                                    transform: 'scale(0.8)'
+                                }}
+                            >
+                                <Card variant="outlined">
+                                    <CardContent>
+                                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                            {post.type}
+                                        </Typography>
+                                        <Typography variant="h5" component="div">
+                                            {post.title}
+                                        </Typography>
+                                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                            {post.body}
+                                        </Typography>
+                                        <Typography variant="body2">
+                                            {'post . author'}
+                                            <br />
+                                            {'---boutons ou options ? --- '}
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Box>
+
+
                         </li>
                     ))}
                 </ul>
-            </div>
+            </div >
+
         </>
     )
 };

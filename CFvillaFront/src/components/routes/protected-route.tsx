cbf6b7a9ff3d11c4cc649dbf6bb22af6bfbd2b3c
Tstassin <Tstassin@users.noreply.github.com>
2024-02-06 10:49:47 +0100

    wip

--- CFvillaFront/src/components/routes/protected-route.tsx
+++ CFvillaFront/src/components/routes/protected-route.tsx
@@ -2,16 +2,12 @@ import React from 'react'
 import { useAuthStore } from '../../store-zustand/authStore'
 import { Navigate } from 'react-router-dom'
 
-
 const ProtectedRoute = (props) => {
-    
-const isAuthenticated =  useAuthStore(state => state.isAuthenticated)
+  const isAuthenticated = useAuthStore(state => state.isAuthenticated)
 
-if (!isAuthenticated) { return <Navigate to='/'/>}
+  if (!isAuthenticated) { return <Navigate to='/' /> }
 
-return   props.children
-    
-  
+  return props.children
 }
 
 export default ProtectedRoute

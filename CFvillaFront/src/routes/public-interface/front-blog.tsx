e42d680405ccfabdffc2f702032492155b544c9b
Samuel <Samueldemees@gmail.com>
2024-02-06 18:45:28 +0100

    suirte

--- CFvillaFront/src/routes/public-interface/front-blog.tsx
+++ CFvillaFront/src/routes/public-interface/front-blog.tsx
@@ -4,21 +4,23 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import CRUD from '../../business/api-requests/CRUD';
-import {Post} from '../../business/types/CRUD.types';
+import { Post } from '../../business/types/CRUD.types';
 import CardMedia from '@mui/material/CardMedia';
 import { Button, CardActionArea, CardActions } from '@mui/material';
+import { Container, Box, Grid } from '@mui/material';
+
 
 
 
 const FrontBlog = () => {
-    const [posts, setpost] = useState<  Post[] >([]);
+    const [posts, setpost] = useState<Post[]>([]);
 
     const fetchPosts = async () => {
-        const posts = await CRUD.getForm('posts/', { populate: ['img', 'author'] })       
+        const posts = await CRUD.getForm('posts/', { populate: ['img', 'author'] })
         if (posts !== undefined && Array.isArray(posts)) {
             setpost(posts)
-          }
-            };
+        }
+    };
 
     useEffect(() => {
         fetchPosts()
@@ -26,19 +28,18 @@ const FrontBlog = () => {
 
     return (
         <>
-            <Card sx={{ width: 1200 }}>
-                <ul>
-                    {posts.map((post ) => (
-                        <li style={{ listStyle: 'none' }}
-                            key={post._id}
-                        >
-                            <CardActionArea>
-                                <CardMedia
-                                    component="img"
-                                    sizes="font-size: 0.125em"
-                                    image={post.img && typeof post.img !== 'string' ? `${import.meta.env.VITE_STATIC_HOST}/static/${post.img.fileName}` : `${import.meta.env.VITE_STATIC_HOST}/static/No-image;jpg`}
-                                    alt={post.img?.toString.name} />  
-                                    <CardContent>                
+            <Box sx={{ flexGrow: 1, height: '100vh', backgroundImage: `url('/static/images/background.jpg')`, backgroundSize: 'cover' }}>
+                <Container sx={{ paddingTop: 4 }}>
+                    <Grid container spacing={2}>
+                        {posts.map((post, index) => (
+                            <Grid item key={index} xs={12} sm={6} md={4}>
+                                <Card sx={{ maxWidth: 345 }}>
+                                    <CardMedia
+                                        sx={{ height: 140 }}
+                                        image={post.img && typeof post.img !== 'string' ? `${import.meta.env.VITE_STATIC_HOST}/static/${post.img.fileName}` : `${import.meta.env.VITE_STATIC_HOST}/static/No-image;jpg`}
+                                        title={post.title}
+                                    />
+                                    <CardContent>
                                         <Typography gutterBottom variant="h5" component="div">
                                             {post.title}
                                         </Typography>
@@ -46,22 +47,20 @@ const FrontBlog = () => {
                                             {post.body}
                                         </Typography>
                                     </CardContent>
-                                
-                                </CardActionArea>
-                            <CardActions>
-                     <Button size="small" color="primary">
-                        Like
-                    </Button>
-                    <Button size="small" color="primary">
-                        Contact
-                    </Button>
-                    </CardActions>
-                        </li>
-                    ))}
-                </ul>
-            </Card>
+                                    <CardActions>
+                                        <Button size="small">Learn More</Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Container>
+            </Box>
+
+
         </>
     )
 };
 
 export default FrontBlog
+//{ }

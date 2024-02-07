7959ceee94c602d7545be400fabeae762232de5c
Samuel <Samueldemees@gmail.com>
2024-02-07 11:32:32 +0100

    structure HTML frontBlog

--- CFvillaFront/src/routes/public-interface/front-blog.tsx
+++ CFvillaFront/src/routes/public-interface/front-blog.tsx
@@ -8,15 +8,10 @@ import { Post } from '../../business/types/CRUD.types';
 import CardMedia from '@mui/material/CardMedia';
 import { Button, CardActionArea, CardActions, rgbToHex } from '@mui/material';
 import { Container, Box, Grid } from '@mui/material';
+
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_API_HOST: string
-  readonly VITE_STATIC_HOST: string
-}
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
+
 
 
 
@@ -37,44 +32,37 @@ const FrontBlog = () => {
 
   return (
     <>
-      <Box sx={{
-        flexGrow: 1,
-        height: '100vh',
-        backgroundImage: (`${import.meta.env.VITE_STATIC_HOST}/static/backGround.jpeg`),
-        backgroundSize: 'cover'
-      }}>
-        <Container sx={{ paddingTop: 4 }}>
-          <Grid container spacing={2}>
-            {posts.map((post, index) => (
-              <Grid item key={index} xs={12} sm={6} md={4}>
-                <Card
-
-                  sx={{
-                    bgcolor: 'darkcyan',
-                    maxWidth: '75%'
-                  }}>
-                  <CardMedia
-                    sx={{ height: 140 }}
-                    image={post.img && typeof post.img !== 'string' ? `${import.meta.env.VITE_STATIC_HOST}/static/${post.img.fileName}` : `${import.meta.env.VITE_STATIC_HOST}/static/No-image.jpg`}
-                    title={post.title}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {post.title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {post.body}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">Learn More</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </Box>
+      <body>
+        <header className='headerBlog'>
+          <div>
+            Entre les hommes et les dieux
+          </div>
+        </header>
+        <main className="mainBlog">
+          <section className='cards'>
+            <div className="card"> CARD1 </div>
+            <div className="card"> CARD2 </div>
+            <div className="card"> CARD3 </div>
+            <div className="card"> CARD4 </div>
+            <div className="card"> CARD5 </div>
+            <div className="card"> CARD6 </div>
+            <div className="card"> CARD7 </div>
+            <div className="card"> CARD8 </div>
+            <div className="card"> CARD9 </div>
+            <div className="card"> CARD10 </div>
+            <div className="card"> CARD11 </div>
+            <div className="card"> CARD12 </div>
+          </section>
+          <section className='aside'>
+            <div className='navBar'>
+              NAV-BAR
+            </div>
+          </section>
+        </main>
+        <footer className='footerBlog'>
+          FOOTER
+        </footer>
+      </body>
 
 
     </>
@@ -83,3 +71,42 @@ const FrontBlog = () => {
 
 export default FrontBlog
 //{ }
+
+// <Box sx={{
+//   flexGrow: 1,
+//   height: '100vh',
+//   backgroundImage: (`${import.meta.env.VITE_STATIC_HOST}/static/backGround.jpeg`),
+//   backgroundSize: 'cover'
+// }}>
+//   <Container sx={{ paddingTop: 4 }}>
+//     <Grid container spacing={2}>
+//       {posts.map((post, index) => (
+//         <Grid item key={index} xs={12} sm={6} md={4}>
+//           <Card
+
+//             sx={{
+//               bgcolor: 'darkcyan',
+//               maxWidth: '75%'
+//             }}>
+//             <CardMedia
+//               sx={{ height: 140 }}
+//               image={post.img && typeof post.img !== 'string' ? `${import.meta.env.VITE_STATIC_HOST}/static/${post.img.fileName}` : `${import.meta.env.VITE_STATIC_HOST}/static/No-image.jpg`}
+//               title={post.title}
+//             />
+//             <CardContent>
+//               <Typography gutterBottom variant="h5" component="div">
+//                 {post.title}
+//               </Typography>
+//               <Typography variant="body2" color="text.secondary">
+//                 {post.body}
+//               </Typography>
+//             </CardContent>
+//             <CardActions>
+//               <Button size="small">Learn More</Button>
+//             </CardActions>
+//           </Card>
+//         </Grid>
+//       ))}
+//     </Grid>
+//   </Container>
+// </Box>

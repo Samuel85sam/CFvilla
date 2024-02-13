6e16caec449b99551e583c93b9bc8d28b188045c
Samuel85sam <samueldemees@gmail.com>
2024-02-13 13:23:27 +0100

    switch pricing/Contact works

--- CFvillaFront/src/routes/public-interface/index-guest.tsx
+++ CFvillaFront/src/routes/public-interface/index-guest.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import FrontBlog from './article-list/front-blog';
 import Header from './header/header';
 import Footer from './footer/footer';
 import '@mantine/core/styles/BackgroundImage.css';
+import Pricing from '../../components/header/pricing';
+import Contact from '../../components/contact/contact';
+import { Link } from './header/header'
+
+
+
+
+const links = [
+
+    { link: 'Contact', label: 'Contact' },
+    { link: 'pricing', label: 'Pricing' },
+
+];
 
 
 
 
 const IndexGuest = () => {
 
+    let [state, setState] = useState(links[0]);
+
+
+
+    useEffect(() => {
+        console.log({ state });
+
+
+    }, [state])
+
+
+   
+
     return (
         <>
             <header>
-                <Header />
+                <Header
+                    state={state}
+                    links={links}
+                    setState={setState} />
             </header>
             <div>
-                <FrontBlog />
+                {state === links[0]?  < Contact /> :  <Pricing />}               
+                 <FrontBlog
+                    state={state}
+                />
             </div>
             <footer>
-            <Footer />
+                <Footer />
             </footer>
         </>
     )
